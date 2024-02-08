953039df9b4601971b1dc08e8e143143402ff9e7
Adrixz03 <122287706+Adrixz03@users.noreply.github.com>
2024-02-08 23:28:28 +0100

    Menu pausa y opciones 2

--- Assets/scripts/Menú/PauseMenu.cs
+++ Assets/scripts/Menú/PauseMenu.cs
@@ -5,11 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    //[SerializeField] private GameObject botonpausa;
     [SerializeField] private GameObject pausemenu;
+
+    private string currentSceneName;
+
+    public static PauseMenu instance;
+
+    void Start()
+    {
+        // Obtener el nombre de la escena actual al inicio
+        currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
+    void Awake() // se hace en el awake para q se inicialice lo primero, por si en el start hubiera algo q lo use y lo usase antes de q se iniciase
+    {
+        if (instance == null)//comprueba si instance no contiene informacion. tambien hace q no se destruya nunca
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //si tiene info, significa q ya existe otro GameManager y destruye este para q no se duplique ya q solo puede haber uno
+            Destroy(gameObject);
+        }
+    }
 
     public void Update()
     {
+        //Si el pausemenu esta abierto que salaga el cursor del mouse sino lo esta desaparezca
+        //Si OptionsPanel esta activo, no se abara el pause menu al darle al escape sino que vuelva atras
         Cursor.lockState = CursorLockMode.None; // Bloquear el cursor del mouse.
 
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -18,19 +43,21 @@ public class PauseMenu : MonoBehaviour
             pausemenu.SetActive(true);
         }
     }
-    public void reanudar()
+
+    public void Reload()
     {
         Time.timeScale = 1f;
         pausemenu.SetActive(false);
     }
 
-    public void reiniciar()
+    public void Restart()
     {
+        //que al reiniciar me cierre el menu
         Time.timeScale = 1f;
-        SceneManager.LoadScene("HabitaciónNiño");
+        SceneManager.LoadScene(currentSceneName);
     }
 
-    public void Salir()
+    public void QuitMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
